2dd6c2032fe2a5fed5d7cfa3a176787067d2a9c8
hlk007 <161023123+hlk007@users.noreply.github.com>
2024-10-30 21:31:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,61 @@
+criaCartao(
+    'ㅤ',
+    'Quais são as duas principais classes de angiospermas?',
+    'Monocotiledôneas (1 cotilédone) e dicotiledôneas (2 cotilédones).'
+)
+criaCartao(
+    'ㅤ',
+    'O que caracteriza as gimnospermas?',
+    'São plantas com sementes expostas, sem frutos, geralmente em estruturas chamadas cones.'
+)
+criaCartao(
+    'ㅤ',
+    'Qual a principal característica das pteridófitas?',
+    'São plantas vasculares que se reproduzem por esporos e não produzem sementes.'
+)
+criaCartao(
+    'ㅤ',
+    'Como se chamam as estruturas onde as pteridófitas produzem esporos?',
+    'Soros, geralmente encontrados na face inferior das folhas.'
+)
+criaCartao(
+    'ㅤ',
+    'O que diferencia as angiospermas das outras plantas?',
+    'Elas possuem flores e produzem frutos que protegem as sementes.'
+)
+criaCartao(
+    'ㅤ',
+    'Por que as briófitas dependem de água para se reproduzir?',
+    'Porque seus gametas masculinos precisam se deslocar na água para fecundar os femininos.'
+)
+criaCartao(
+    'ㅤ',
+    'Dê um exemplo comum de gimnosperma.',
+    'Araucária'
+)
+criaCartao(
+    'ㅤ',
+    'Quais são os principais grupos de briófitas?',
+    'Musgos, hepáticas e antóceros.'
+)
+criaCartao(
+    'ㅤ',
+    'Qual a principal diferença entre bactérias e vírus??',
+    ' As bactérias são seres vivos com metabolismo próprio, enquanto os vírus não têm metabolismo e dependem de hospedeiros para se reproduzir.'
+)
+criaCartao(
+    'ㅤ',
+    'Os vírus são considerados seres vivos? Por quê?',
+    'Não, porque eles não têm metabolismo próprio e precisam de uma célula hospedeira para se reproduzir.'
+)
+criaCartao(
+    'ㅤ',
+    'Qual é a estrutura básica de um vírus?',
+    'Um ácido nucleico (DNA ou RNA) envolto por uma cápsula de proteínas, chamada capsídeo.'
+)
+criaCartao(
+    'ㅤ',
+    'O que são bactérias autotróficas?',
+    'São bactérias capazes de produzir seu próprio alimento, geralmente por fotossíntese ou quimiossíntese.'
+)
+
